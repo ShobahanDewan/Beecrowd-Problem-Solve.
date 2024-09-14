3b6dfd462fb6ea69f27fa159019e3f2900d477a3
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-14 22:57:22 +0600

    1036 - Bhaskara's Formula

---
+++ 1036 - Bhaskara's Formula.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double a, b, c, pr1;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    pr1 = (b*b) - (4*(a)*(c));
+
+    if(pr1 > 0 && a != 0)
+    {
+        double x, y;
+        pr1 = sqrt(pr1);
+        x = (-b + pr1)/(2*a);
+        y = (-b - pr1)/(2*a);
+        printf("R1 = %.5lf\n", x);
+        printf("R2 = %.5lf\n", y);
+    }
+    else
+    {
+        printf("Impossivel calcular\n");
+    }
+
+    return 0;
+}
