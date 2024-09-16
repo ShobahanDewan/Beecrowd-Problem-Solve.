15472f13c997006b12ee95cef97c42ee0859dd47
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-16 21:01:33 +0600

    1038 - Snack

---
+++ 1038 - Snack.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+
+{
+    int x,y;
+    float p=0;
+    scanf("%d %d",&x,&y);
+    if(x==1)
+    {
+        p = (float)(4.00 * y);
+    }
+    else if(x==2)
+    {
+        p = (float)(4.50 * y);
+    }
+    else if(x==3)
+    {
+        p = (float)(5.00 * y);
+    }
+    else if(x==4)
+    {
+        p = (float)(2.00 * y);
+    }
+    else if(x==5)
+    {
+        p = (float)(1.50 * y);
+    }
+    printf("Total: R$ %0.2f\n",p);
+    return 0;
+}
