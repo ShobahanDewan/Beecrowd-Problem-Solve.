f256cbc5b7a4641344a90cff7a9101d680decd7d
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-23 23:51:42 +0600

    1047 - Game Time with Minutes

--- 1047 - Game Time with Minutes.c
+++ 1047 - Game Time with Minutes.c
@@ -4,6 +4,7 @@ int main()
     int a, b, c, d, k;
     scanf("%d %d %d %d", &a, &c, &b, &d);
     k = ((b*60)+d) - ((a*60)+c);
+    
     if(k<=0) k += 24*60;
     printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", k/60, k%60);
     return 0;
