2a61e81d045c435125f06cd7b2eee874deae3c1d
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-10-31 23:29:55 +0600

    1015 - Distance Between Two Points

--- 1015 - Distance Between Two Points.c
+++ 1015 - Distance Between Two Points.c
@@ -5,7 +5,6 @@ int main()
     double x1,y1,x2,y2,D;
     scanf("%lf %lf \n%lf %lf",&x1,&y1,&x2,&y2);
     D = sqrt((pow((x2-x1),2))+(pow((y2-y1),2)));
-    
     printf("%0.4lf\n",D);
     return 0;
 }
