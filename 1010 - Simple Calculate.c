8f6c1a45ecec82505257524554d3d818191e0c12
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-01-06 21:45:43 +0600

    1010 - Simple Calculate

--- 1010 - Simple Calculate.c
+++ 1010 - Simple Calculate.c
@@ -5,6 +5,7 @@ int main()
     float price_1,price_2,paid;
     scanf("%d %d %f",&product_code_1,&unit_1,&price_1);
     scanf("\n%d %d %f",&product_code_2,&unit_2,&price_2);
+    
     paid=(unit_1*price_1)+(unit_2*price_2);
     printf("VALOR A PAGAR: R$ %0.2f\n",paid);
     return 0;
