e068b068402ccb53b1416fe3230061d281b4dfba
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-04-07 15:51:05 +0600

    1008 - Salary

--- 1008 - Salary.c
+++ 1008 - Salary.c
@@ -6,7 +6,6 @@ int main()
    float salary_per_hour, total_salary;
    scanf("%d %d %f",&num, &work_hour, &salary_per_hour);
    total_salary = work_hour * salary_per_hour;
-
    printf("NUMBER = %d\n",num);
    printf("SALARY = U$ %0.2f\n",total_salary);
    return 0;
