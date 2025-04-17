c31436ca06042d122a0317d020c5e986fa65ae35
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-04-17 22:01:04 +0600

    1009 - Salary with Bonus

--- 1009 - Salary with Bonus.c
+++ 1009 - Salary with Bonus.c
@@ -4,7 +4,6 @@ int main()
     char First_Name[50];
     double fixed_salary ,total_sales,total_salary;
     scanf("%s",&First_Name);
-    
     scanf("%lf %lf",&fixed_salary,&total_sales); 
     total_salary = (total_sales*0.15)+fixed_salary;
     printf("TOTAL = R$ %0.2lf\n",total_salary);
